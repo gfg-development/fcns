f86a87e62003c8330701ab774f8fd0628da3d0bd
Gerrit Grutzeck <g.grutzeck@gfg-development.de>
2018-03-07 11:35:31 +0100

    Added Arithmetic Combinator

---
+++ src/ArithmeticCombinator.ts
@@ -0,0 +1,179 @@
+import { Module, SignalCollection } from './Module'
+import { Signals, SpecialSignals, getSignalCount, mergeSignals } from './Signals'
+
+export enum Operations {
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+  AND,
+  OR,
+  XOR,
+  LSHIFT,
+  RSHIFT,
+  EXP
+}
+
+export class ArithmeticCombinator extends Module {
+  private operation: Operations
+  private leftOperand: string | SpecialSignals | number
+  private rightOperand: string | number
+  private outputType: string | SpecialSignals
+  private inputs: Signals = {}
+  private outputs: Signals = {}
+
+  constructor(
+    inputSignals: SignalCollection,
+    outputSignals: SignalCollection,
+    leftOperand: string | SpecialSignals,
+    operation: Operations,
+    rightOperand: string | SpecialSignals | number,
+    output: string | SpecialSignals
+  ) {
+    super()
+
+    // check for the right combination of operands and output
+    if (output === SpecialSignals.EACH && leftOperand !== SpecialSignals.EACH) {
+      throw new Error('ArithmeticCombinator: output can only be each if input is also each!')
+    }
+
+    if (output === SpecialSignals.ANYTHING) {
+      throw new Error('ArithmeticCombinator: output can not be anything')
+    }
+
+    if (output === SpecialSignals.EVERYTHING) {
+      throw new Error('ArithmeticCombinator: output can not be everything')
+    }
+
+    if (output === SpecialSignals.ANYTHING) {
+      throw new Error('ArithmeticCombinator: input can not be anything')
+    }
+
+    if (output === SpecialSignals.EVERYTHING) {
+      throw new Error('ArithmeticCombinator: input can not be everything')
+    }
+
+    this.operation = operation
+    this.leftOperand = leftOperand
+    this.rightOperand = rightOperand
+    this.outputType = output
+
+    this.connectInput(inputSignals.red, 'redInput')
+    this.connectInput(inputSignals.green, 'greenInput')
+
+    this.connectOutput(outputSignals.red, 'redOutput')
+    this.connectOutput(outputSignals.green, 'greenOutput')
+  }
+
+  tick(): void {
+    let green = this.signalNetworkManager.getSignals('greenInput')
+    let red = this.signalNetworkManager.getSignals('redInput')
+    this.inputs = mergeSignals(red, green)
+    this.outputs = {}
+  }
+
+  tock(): void {
+    const rightOperand: number = this.operandToNumber(this.rightOperand)
+
+    if (this.leftOperand === SpecialSignals.EACH) {
+      // handle special signal each
+      for (const name of Object.keys(this.inputs)) {
+        const leftOperand = this.operandToNumber(name)
+        const result = this.evaluateOperation(leftOperand, rightOperand)
+
+        if (this.outputType === SpecialSignals.EACH) {
+          this.addSignalToOutput(name, result)
+        } else {
+          this.addSignalToOutput(this.outputType as string, result)
+        }
+      }
+    } else {
+      const leftOperand = this.operandToNumber(this.leftOperand)
+      const result = this.evaluateOperation(leftOperand, rightOperand)
+      this.addSignalToOutput(this.outputType as string, result)
+    }
+
+    this.signalNetworkManager.addSignals('redOutput', this.outputs)
+    this.signalNetworkManager.addSignals('greenOutput', this.outputs)
+  }
+
+  private operandToNumber(operand: string | SpecialSignals | number): number {
+    switch (typeof operand) {
+      case 'number':
+        return operand as number
+
+      case 'string':
+        return getSignalCount(operand as string, this.inputs)
+
+      default:
+        throw new Error('DeciderCombinator: operand not string or number!')
+    }
+  }
+
+  private evaluateOperation(leftOperand: number, rightOperand: number): number {
+    switch (this.operation) {
+      case Operations.ADD:
+        return leftOperand + rightOperand
+
+      case Operations.SUB:
+        return leftOperand - rightOperand
+
+      case Operations.MUL:
+        return leftOperand * rightOperand
+
+      case Operations.DIV:
+        if (rightOperand === 0) {
+          return 0
+        } else {
+          return Math.floor(leftOperand / rightOperand)
+        }
+
+      case Operations.MOD:
+        if (rightOperand === 0) {
+          return 0
+        } else {
+          return leftOperand % rightOperand
+        }
+
+      case Operations.EXP:
+        if (rightOperand >= 0) {
+          return 0
+        } else {
+          return Math.pow(leftOperand, rightOperand)
+        }
+
+      case Operations.LSHIFT:
+        if (rightOperand >= 0) {
+          return leftOperand * Math.pow(2, rightOperand)
+        } else {
+          return 0
+        }
+
+      case Operations.RSHIFT:
+        if (rightOperand >= 0) {
+          return Math.floor(leftOperand / Math.pow(2, rightOperand))
+        } else {
+          return 0
+        }
+
+      case Operations.AND:
+        return leftOperand & rightOperand
+
+      case Operations.OR:
+        return leftOperand | rightOperand
+
+      case Operations.XOR:
+        return leftOperand ^ rightOperand
+
+      default:
+        throw new Error('ArithmeticCombinator: unknown operation!')
+    }
+  }
+
+  private addSignalToOutput(signalName: string, count: number) {
+    let newSignals: Signals = {}
+    newSignals[signalName] = count
+    this.outputs = mergeSignals(this.outputs, newSignals)
+  }
+}
---
+++ test/ArithmeticCombinator.test.ts
@@ -0,0 +1,255 @@
+import { SignalNetwork } from '../src/SignalNetwork'
+import { Signals, SpecialSignals } from '../src/Signals'
+import { ArithmeticCombinator, Operations } from '../src/ArithmeticCombinator'
+
+describe('ArithmeticCombinator', () => {
+  it('With empty inputs, it outputs nothing', () => {
+    let combinator = new ArithmeticCombinator(
+      { red: 'redIn', green: 'greenIn' },
+      { red: 'redOut', green: 'greenOut' },
+      SpecialSignals.EACH,
+      Operations.ADD,
+      1,
+      SpecialSignals.EACH
+    )
+
+    combinator.tick()
+    combinator.clearNetworks()
+    combinator.tock()
+    expect(combinator.getOutputSignals('greenOut')).toEqual({})
+    expect(combinator.getOutputSignals('redOut')).toEqual({})
+  })
+
+  describe('Simple operations', () => {
+    it('Simple addition', () => {
+      const red: Signals = {
+        COPPER_PLATE: 10
+      }
+
+      const output: Signals = {
+        COPPER_PLATE: 15
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        'COPPER_PLATE',
+        Operations.ADD,
+        5,
+        'COPPER_PLATE'
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('Division with a reminder', () => {
+      const red: Signals = {
+        COPPER_PLATE: 15
+      }
+
+      const green: Signals = {
+        IRON_PLATE: 10
+      }
+
+      const output: Signals = {
+        IRON_PLATE: 1
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        'COPPER_PLATE',
+        Operations.DIV,
+        'IRON_PLATE',
+        'IRON_PLATE'
+      )
+
+      combinator.addInputSignals('redIn', red)
+      combinator.addInputSignals('greenIn', green)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('Division with a reminder', () => {
+      const red: Signals = {
+        COPPER_PLATE: 16
+      }
+
+      const green: Signals = {
+        IRON_PLATE: 10
+      }
+
+      const output: Signals = {
+        IRON_PLATE: 1
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        'COPPER_PLATE',
+        Operations.DIV,
+        'IRON_PLATE',
+        'IRON_PLATE'
+      )
+
+      combinator.addInputSignals('redIn', red)
+      combinator.addInputSignals('greenIn', green)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('Division by zero', () => {
+      const red: Signals = {
+        COPPER_PLATE: 16
+      }
+
+      const output: Signals = {}
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        'COPPER_PLATE',
+        Operations.DIV,
+        'IRON_PLATE',
+        'IRON_PLATE'
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+  })
+
+  describe('each', () => {
+    it('simple addition', () => {
+      const red: Signals = {
+        COPPER_PLATE: 10
+      }
+
+      const output: Signals = {
+        COPPER_PLATE: 15
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        SpecialSignals.EACH,
+        Operations.ADD,
+        5,
+        SpecialSignals.EACH
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('multiple additions', () => {
+      const red: Signals = {
+        COPPER_PLATE: 10,
+        IRON_PLATE: 15
+      }
+
+      const output: Signals = {
+        COPPER_PLATE: 15,
+        IRON_PLATE: 20
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        SpecialSignals.EACH,
+        Operations.ADD,
+        5,
+        SpecialSignals.EACH
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('adding each with one signal', () => {
+      const red: Signals = {
+        COPPER_PLATE: 10,
+        IRON_PLATE: 1
+      }
+
+      const output: Signals = {
+        COPPER_PLATE: 11,
+        IRON_PLATE: 2
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        SpecialSignals.EACH,
+        Operations.ADD,
+        'IRON_PLATE',
+        SpecialSignals.EACH
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+
+    it('adding each with one signal and output to one signal', () => {
+      const red: Signals = {
+        COPPER_PLATE: 10,
+        IRON_PLATE: 1
+      }
+
+      const output: Signals = {
+        IRON_PLATE: 13
+      }
+
+      let combinator = new ArithmeticCombinator(
+        { red: 'redIn', green: 'greenIn' },
+        { red: 'redOut', green: 'greenOut' },
+        SpecialSignals.EACH,
+        Operations.ADD,
+        'IRON_PLATE',
+        'IRON_PLATE'
+      )
+
+      combinator.addInputSignals('redIn', red)
+
+      combinator.tick()
+      combinator.clearNetworks()
+      combinator.tock()
+      expect(combinator.getOutputSignals('greenOut')).toEqual(output)
+      expect(combinator.getOutputSignals('redOut')).toEqual(output)
+    })
+  })
+})
